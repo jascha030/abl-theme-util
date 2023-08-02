86751f0af0ed6db3a1d7552624581dfe54516f2f
Jascha030 <contact@jaschavanaalst.nl>
2023-08-02 23:51:33 +0200

    ♻️ Refactor Application to get name and version from Container.

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -4,18 +4,37 @@ declare(strict_types=1);
 
 namespace Jascha030\LiveTheme\Console;
 
+use Psr\Container\ContainerInterface;
+use RuntimeException;
 use Symfony\Component\Console\Application as BaseApplication;
 
 final class Application extends BaseApplication
 {
-    private const APP_VERSION = '1.0.0';
-
-    private const APP_NAME = 'ABL Theme Util';
-
-    public function __construct()
+    public function __construct(private ContainerInterface $container)
     {
-        parent::__construct(self::APP_NAME, self::APP_VERSION);
+        $version = $this->container->get('app.version');
+        $name    = $this->container->get('app.name');
+
+        if (! is_string($name) || ! is_string($version)) {
+            throw self::getException($name, $version);
+        }
+
+        parent::__construct($name, $version);
     }
 
+    private static function getException(mixed $name, mixed $version): RuntimeException
+    {
+        $invalidKeys = array_keys(array_filter(
+            [
+                'app.name'    => $name,
+                'app.version' => $version,
+            ],
+            static fn ($v) => ! is_string($v)
+        ));
 
+        return new \RuntimeException(sprintf(
+            'Invalid value type for container binding(s): "%s", binding(s) should be of type "string".',
+            implode(', ', $invalidKeys)
+        ));
+    }
 }
