a9ebb0c3e0bde5a4ca41741935dbe7b8f8302f94
Jascha030 <contact@jaschavanaalst.nl>
2023-08-03 23:50:50 +0200

    ✨ Add method to check current notation type to ColorValue.

--- src/Model/ColorValue.php
+++ src/Model/ColorValue.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Jascha030\LiveTheme\Model;
 
+use Jascha030\LiveTheme\Model\Theme\ColorNotation;
+use Jascha030\LiveTheme\Util\Color;
+use Symfony\Component\Serializer\Annotation\Ignore;
 use Symfony\Component\Serializer\Annotation\SerializedName;
+use function array_map;
+use function count;
+use function is_float;
+use function is_numeric;
 
 class ColorValue extends Value
 {
@@ -66,5 +73,63 @@ class ColorValue extends Value
         $this->alpha = $alpha;
 
         return $this;
+    }
+
+    #[Ignore]
+    public function getType(): ColorNotation
+    {
+        $numeric = array_map(
+            static fn(?Value $v) => null !== $v && is_numeric($v->getValue()),
+            [
+                $this->getRed(),
+                $this->getGreen(),
+                $this->getBlue(),
+                $this->getAlpha(),
+            ]
+        );
+
+        if (count($numeric) >= 4) {
+            return ColorNotation::RGBA;
+        }
+
+        if (count($numeric) === 3) {
+            return ColorNotation::RGB;
+        }
+
+        if ($this->getValue() !== null && str_starts_with($this->getValue(), '#')) {
+            return ColorNotation::HEX;
+        }
+    }
+
+    public function asHex(): Value
+    {
+        $v = new Value();
+
+        $hex = match (true) {
+            is_float($this->getValue()),
+                is_numeric($this->getValue())
+                && !str_starts_with((string)$this->getValue(), '#') => $this->getValue(),
+            $this->getType() === ColorNotation::HEX                 => $this->getValue(),
+            $this->getType() === ColorNotation::RGBA                => Color::rgbaToHex(
+                $this->getRed()?->getValue(),
+                $this->getGreen()?->getValue(),
+                $this->getBlue()?->getValue(),
+                $this->getAlpha()?->getValue()
+            ),
+            $this->getType() === ColorNotation::RGB                 => Color::rgbToHex(
+                $this->getRed()?->getValue(),
+                $this->getGreen()?->getValue(),
+                $this->getBlue()?->getValue()
+            ),
+            default                                                 => null,
+        };
+
+        if ($hex === null) {
+            throw new \InvalidArgumentException('Invalid color value type, could not be converted to hex.');
+        }
+
+        $v->setValue($hex);
+
+        return $v;
     }
 }
