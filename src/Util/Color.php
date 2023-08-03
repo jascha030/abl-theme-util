143e37cf5167f2feddf9ba4b38b916ed95108280
Jascha030 <contact@jaschavanaalst.nl>
2023-08-03 23:52:45 +0200

    ♻️ More lenient types and add rgba util method.

--- src/Util/Color.php
+++ src/Util/Color.php
@@ -10,22 +10,31 @@ use function strlen;
 
 final class Color
 {
-    public static function rgbToHex(string $r, string $g, string $b): string
+    public static function rgbToHex(string|int $r, string|int $g, string|int $b): string
     {
         return sprintf('#%02x%02x%02x', $r, $g, $b);
     }
 
+    public static function rgbaToHex(string|int $r, string|int $g, string|int $b, string|int $a): string
+    {
+        if ((string)$a === '255') {
+            return self::rgbToHex($r, $g, $b);
+        }
+
+        return sprintf('#%02x%02x%02x%02x', $r, $g, $b, $a);
+    }
+
     /**
-     * @return array<string, int|null>
+     * @return array<string|int, int|null>
      */
-    public static function hexToRGB(string $hex): array
+    public static function hexToRgb(string $hex): array
     {
         if (! str_starts_with($hex, '#') && 6 === strlen($hex)) {
             $hex = '#' . $hex;
         }
 
         if (null === sscanf($hex, '#%02x%02x%02x')) {
-            throw new RuntimeException('Invalid format for string: "' . $hex . '".');
+            throw new RuntimeException('Invalid format for string|int: "' . $hex . '".');
         }
 
         [$r, $g, $b] = sscanf($hex, '#%02x%02x%02x');
